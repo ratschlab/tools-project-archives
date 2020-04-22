f1d74ce783e20e715b845f7d069e6baa7db59ad1
Noah Fleischmann <noahfleischmann21@gmail.com>
2020-04-22 17:20:11 +0200

    Refactor path handling using pathlib

--- archiver/archiver.py
+++ archiver/archiver.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 import hashlib
+from pathlib import Path
 
 # TODO: Validate arguments (valid paths)
 # TODO: Get real path (could be with absolute, relative, with or without slash at the end)
@@ -12,9 +13,18 @@ import hashlib
 
 def archive(args):
     # Path to a file or directory for which to create the archive
-    input_path = args.input
+    input_path = Path(args.input)
     # Path to a directory which will be created (if it does yet exist)
     output_directory_path = args.output
+
+    # Argparse already checks if argument is present, so only argument format needs to be validated
+    terminate_if_path_nonexistent(input_path)
+
+    if output_directory_path:
+        output_directory_path = Path(output_directory_path)
+    else:
+        modified_path = input_path.absolute().as_posix() + "-archive"
+        output_directory_path = Path(modified_path)
 
     create_directory_if_nonexistent(output_directory_path)
     create_file_listing_hash(input_path, output_directory_path)
@@ -23,46 +33,41 @@ def archive(args):
     compress_using_lzip(output_directory_path)
     create_archive_hash(output_directory_path)
 
-    print("Archive created: " + output_directory_path)
-
-
-def create_directory_if_nonexistent(directory_path):
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
+    print("Archive created: " + output_directory_path.as_posix())
 
 
 # TODO: parallelization
 def create_file_listing_hash(input_path, output_directory_path):
-    for root, subdirs, files in os.walk(input_path):
+    for root, _, files in os.walk(input_path):
         hashes = []
         for file in files:
             # Read file content as binary for hash
             with open(os.path.join(root, file), "rb") as read_file:
                 hashes.append([root, file, hashlib.md5(read_file.read()).hexdigest()])
 
-        write_hash_list_to_file(os.path.join(output_directory_path + "archive.md5"), hashes)
+        write_hash_list_to_file(Path.joinpath(output_directory_path, "archive.md5"), hashes)
 
 
 def create_tar_archive(input_path, output_directory_path):
-    path = os.path.join(output_directory_path, "archive.tar")
+    path = Path.joinpath(output_directory_path, "archive.tar")
     subprocess.run(["tar", "-cf", path, input_path])
 
 
 def create_archive_listing(input_path, output_directory_path):
-    listing_path = os.path.join(output_directory_path, "archive.tar.lst")
-    tar_path = os.path.join(output_directory_path, "archive.tar")
+    listing_path = Path.joinpath(output_directory_path, "archive.tar.lst")
+    tar_path = Path.joinpath(output_directory_path, "archive.tar")
 
     archive_listing_file = open(listing_path, "w")
     subprocess.run(["tar", "-tvf", tar_path], stdout=archive_listing_file)
 
 
 def compress_using_lzip(output_directory_path):
-    path = os.path.join(output_directory_path, "archive.tar")
+    path = Path.joinpath(output_directory_path, "archive.tar")
     subprocess.run(["plzip", path])
 
 
 def create_archive_hash(output_directory_path):
-    path = os.path.join(output_directory_path, "archive.tar.lz")
+    path = Path.joinpath(output_directory_path, "archive.tar.lz")
 
     hasher = hashlib.md5()
     # Read file content as binary for hash
@@ -70,7 +75,7 @@ def create_archive_hash(output_directory_path):
         buf = read_file.read()
         hasher.update(buf)
 
-    hash_file = open(output_directory_path + "archive.tar.lz.md5", "w")
+    hash_file = open(Path.joinpath(output_directory_path, "archive.tar.lz.md5"), "w")
     hash_file.write(hasher.hexdigest())
 
 
@@ -79,3 +84,13 @@ def write_hash_list_to_file(file_path, hashes):
     for file in hashes:
         path = os.path.join(file[0], file[1])
         hash_file.write(file[2] + " " + path + "\n")
+
+
+def terminate_if_path_nonexistent(path):
+    if not path.exists():
+        sys.exit("Path does not exist: " + path.absolute())
+
+
+def create_directory_if_nonexistent(path):
+    if not path.exists():
+        path.mkdir()
