67934dd44ff138dd7b7077725c8fe2dac2d9e7d5
Noah Fleischmann <noahfleischmann21@gmail.com>
2020-05-11 18:31:27 +0200

    Ignore line brakes for hash files listing tests

--- tests/test_archiver.py
+++ tests/test_archiver.py
@@ -29,13 +29,13 @@ def test_create_archive(tmp_path):
     assert compare_listing_files(archive_path.joinpath("test-folder.tar.lst"), tmp_path.joinpath("test-folder.tar.lst"))
 
     # Test content md5 tar
-    assert filecmp.cmp(archive_path.joinpath("test-folder.tar.md5"), tmp_path.joinpath("test-folder.tar.md5"))
+    assert compare_text_file(archive_path.joinpath("test-folder.tar.md5"), tmp_path.joinpath("test-folder.tar.md5"))
 
     # Test content md5 tar.lz
-    assert filecmp.cmp(archive_path.joinpath("test-folder.tar.lz.md5"), tmp_path.joinpath("test-folder.tar.lz.md5"))
+    assert compare_text_file(archive_path.joinpath("test-folder.tar.lz.md5"), tmp_path.joinpath("test-folder.tar.lz.md5"))
 
     # Assert content md5 of archive content
-    assert filecmp.cmp(archive_path.joinpath("test-folder.md5"), tmp_path.joinpath("test-folder.md5"))
+    assert compare_text_file(archive_path.joinpath("test-folder.md5"), tmp_path.joinpath("test-folder.md5"))
 
 
 def test_extract_archive(tmp_path):
@@ -120,17 +120,26 @@ def get_folder_path():
     dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
     return Path(os.path.join(dir_path, "test-ressources/test-folder"))
 
+def compare_text_file(file_a_path, file_b_path):
+    try:
+        with open(file_a_path, "r") as file1, open(file_b_path, "r") as file2:
+            return file1.read().rstrip() == file2.read().rstrip()
+    except:
+        return False
 
 def compare_listing_files(listing_file_path_a, listing_file_path_b):
-    with open(listing_file_path_a, "r") as file1, open(listing_file_path_b, "r") as file2:
-        return compare_listing_text(file1.read(), file2.read())
+    try:
+        with open(listing_file_path_a, "r") as file1, open(listing_file_path_b, "r") as file2:
+            return compare_listing_text(file1.read(), file2.read())
+    except:
+        return False
 
 
-def compare_listing_text(listing1, listing2):
-    listing1_path_array = get_array_of_last_multiline_text_parts(listing1)
-    listing2_path_array = get_array_of_last_multiline_text_parts(listing2)
+def compare_listing_text(listing_a, listing_b):
+    listing_a_path_array = get_array_of_last_multiline_text_parts(listing_a)
+    listing_b_path_array = get_array_of_last_multiline_text_parts(listing_b)
 
-    return listing1_path_array == listing2_path_array
+    return listing_a_path_array == listing_b_path_array
 
 
 def get_array_of_last_multiline_text_parts(multiline_text):
