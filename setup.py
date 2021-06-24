86e7b469e7aa3baf2c0b633285043fd179c40e9f
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2021-06-24 16:15:35 +0200

    Bump version: 0.3.0 → 0.3.1-dev1

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url="https://github.com/ratschlab/tools-project-archives",
-    version='0.3.0',
+    version='0.3.1-dev1',
     zip_safe=False,
 )
