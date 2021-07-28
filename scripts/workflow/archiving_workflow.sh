91dea9239a21e6160fd1f48e41da98c2a4caf170
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2021-07-28 14:27:37 +0200

    simple snakemake workflow to easily archive larger directories

---
+++ scripts/workflow/archiving_workflow.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+usage()
+{
+    echo ""
+    echo "Running archiving workflow (create filelist, create tar and create compressed-tar) on a cluster or locally"
+    echo "Usage: $0 [-h] [-l] [-m MIN_WORKERS] [-n MAX_WORKERS [-p PART_SIZE] [-w WORKDIR] DIR_TO_BE_ARCHIVED DEST_DIR"
+    echo "  -h  Help. Display this message and quit."
+    echo "  -l  Run locally on current host instead of submitting to cluster"
+    echo "  -m  minimum number of workers, typically set for IO bound tasks"
+    echo "  -n  maximum number of workers, typically set for CPU bound tasks like compression"
+    echo "  -p  size of an archive part, see archiver archive --help"
+    echo "  -w  working directory"
+    echo ""
+}
+
+WORKDIR_OPT=""
+RUN_LOCALLY=0 # by default, use invoke workflow with --cluster
+
+while getopts 'hlm:n:p:w:' opt; do
+    case $opt in
+        (h)
+            usage
+            exit 0
+            ;;
+        (l)   RUN_LOCALLY=1;;
+        (m)   MIN_WORKERS=$OPTARG;;
+        (n)   MAX_WORKERS=$OPTARG;;
+        (p)   PART_SIZE=$OPTARG;;
+        (w)   WORKDIR_OPT="wdir_root=${OPTARG}";;
+        (*)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+shift "$(($OPTIND -1))"
+
+ARGS="$@"
+
+if [ "$#" -lt 2 ]; then
+    echo "need source and destination arguments!"
+    usage
+    exit 1
+fi
+
+SRC_DIR=$1
+ARCHIVE_DIR=$2
+shift 2
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SNAKEFILE=${SCRIPT_DIR}/Snakefile
+
+source ${SCRIPT_DIR}/archiving_workflow_env.sh
+
+NR_JOBS=1 # value for --jobs parameter of snakemake
+if [ ${RUN_LOCALLY} ]; then
+    SK_CLUSTER_CMD=""
+    NR_JOBS=${MAX_WORKERS} # in local execution --jobs is an alias for --cores
+fi
+
+if [ -z "${WORKDIR_OPT}" ]; then
+    if [ -z "${DEFAULT_WORKDIR}" ]; then
+        WORKDIR_OPT="wdir_root=${WORKDIR_OPT}"
+    else
+        WORKDIR_OPT="wdir_root=/tmp"
+    fi
+fi
+
+
+snakemake --cluster "${SK_CLUSTER_CMD}" --config src_dir=${SRC_DIR} archive_dir=${ARCHIVE_DIR} ${WORKDIR_OPT} --jobs ${NR_JOBS} --keep-going --printshellcmds ${SK_ADDITIONAL_OPTS} $@
+
+
---
+++ scripts/workflow/archiving_workflow_env.sh
@@ -0,0 +1,14 @@
+# Adapt the following variables to your environment
+
+export ARCHIVER_MAX_CPUS_ENV_VAR="LSB_MAX_NUM_PROCESSORS" 
+
+# '--cluster' parameter for snakemake
+SK_CLUSTER_CMD='bsub -J archiving_{rule} -W "{resources.hours}:00" -n {threads} -R "rusage[mem={resources.mem_mb}]" -R "span[hosts=1]" -o logs/{rule}_%J.out -e logs/{rule}_%J.err'
+
+# aditional options or snakemake
+SK_ADDITIONAL_OPTS="--envvars ARCHIVER_MAX_CPUS_ENV_VAR --latency-wait 10 --default-resources hours=120 "
+
+# default work directory, if user doesn't set one
+# Has to be on a distributed filesytem in case of a cluster submission
+DEFAULT_WORKDIR="${HOME}/tmp/tmp_archiving"
+
