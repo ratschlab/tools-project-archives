eaa2e41588b1b17063390a9371fc91b03a6d3101
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2021-08-05 16:56:24 +0200

    clean ups

--- scripts/workflow/archiving_workflow_env.sh
+++ scripts/workflow/archiving_workflow_env.sh
@@ -1,11 +1,11 @@
-# Adapt the following variables to your environment
+# Adapt the following variables to your environment as needed
 
 export ARCHIVER_MAX_CPUS_ENV_VAR="LSB_MAX_NUM_PROCESSORS" 
 
 # '--cluster' parameter for snakemake
 SK_CLUSTER_CMD='bsub -J archiving_{rule} -W "{resources.hours}:00" -n {threads} -R "rusage[mem={resources.mem_mb}]" -R "span[hosts=1]" -o logs/{rule}_%J.out -e logs/{rule}_%J.err'
 
-# aditional options or snakemake
+# additional options for snakemake
 SK_ADDITIONAL_OPTS="--envvars ARCHIVER_MAX_CPUS_ENV_VAR --latency-wait 20 --default-resources hours=120 "
 
 # default work directory, if user doesn't set one
